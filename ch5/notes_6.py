f44b5fcc60364155fe69405bf4bedf25dc2876c7
xSakix <seckarovci@gmail.com>
2018-06-15 17:48:57 +0200

    notes on bayesian analysis from ch5

--- ch5/notes_6.py
+++ ch5/notes_6.py
@@ -13,19 +13,49 @@ d['Marriage_s'] = (d.Marriage - np.mean(d.Marriage)) / np.std(d.Marriage)
 d['MedianAgeMarriage_s'] = (d.MedianAgeMarriage - np.mean(d.MedianAgeMarriage)) / np.std(d.MedianAgeMarriage)
 
 with pm.Model() as model:
-    sigma = pm.Uniform('sigma', lower=0, upper=10)
-    bA = pm.Normal('bA', mu=0, sd=1)
-    bR = pm.Normal('bR', mu=0, sd=1)
     a = pm.Normal('a', mu=10, sd=10)
-    mu = pm.Deterministic('mu', a + bR * d.Marriage_s + bA * d.MedianAgeMarriage_s)
+    bA = pm.Normal('bA', mu=0, sd=1, shape=2)
+    sigma = pm.Uniform('sigma', lower=0, upper=10)
+    mu = pm.Deterministic('mu', a + bA[0] * d.Marriage_s + bA[1] * d.MedianAgeMarriage_s)
     divorce = pm.Normal('divorce', mu=mu, sd=sigma, observed=d.Divorce)
-    trace_model = pm.sample(1000, tune=1000)
+    trace_5_3 = pm.sample(1000, tune=1000)
 
-mu_pred = trace_model['mu']
+trace_df = pm.trace_to_dataframe(trace_5_3)
+print(trace_df[['a', 'bA__0', 'bA__1']].corr().round(2))
+
+mu_pred = trace_5_3['mu']
 mu_hpd = pm.hpd(mu_pred, alpha=0.05)
-divorce_pred = pm.sample_ppc(trace_model, model=model, samples=1000)['divorce']
+divorce_pred = pm.sample_ppc(trace_5_3, model=model, samples=1000)['divorce']
 divorce_hpd = pm.hpd(divorce_pred)
 
 plt.errorbar(d.Divorce, divorce_pred.mean(0), yerr=np.abs(divorce_pred.mean(0) - mu_hpd.T), fmt='C0o')
-plt.plot(d.Divorce,divorce_pred.mean(0),'C0o')
+plt.plot(d.Divorce, divorce_pred.mean(0), 'C0o')
+plt.xlabel('observed divorce')
+plt.ylabel('predicted divorce')
+min_x, max_x = d.Divorce.min(), d.Divorce.max()
+plt.plot([min_x, max_x], [min_x, max_x], 'k--')
+plt.show()
+
+plt.figure(figsize=(10, 12))
+
+residuals = d.Divorce - mu_pred.mean(0)
+idx = np.argsort(residuals)
+y_label = d.Loc[idx]
+y_points = np.linspace(0, 1, 50)
+
+plt.errorbar(residuals[idx],
+             y_points,
+             xerr=np.abs(divorce_pred.mean(0) - mu_hpd.T),
+             fmt='C0o',
+             lw=3)
+
+plt.errorbar(residuals[idx],
+             y_points,
+             xerr=np.abs(divorce_pred.mean(0) - divorce_hpd.T),
+             fmt='C0o',
+             lw=3,
+             alpha=0.5)
+
+plt.yticks(y_points, y_label)
+plt.vlines(0, 0, 1, 'grey')
 plt.show()
